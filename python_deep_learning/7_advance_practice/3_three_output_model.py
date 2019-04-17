fac545504d49a4dc128a18a3c81e52d51b9f88b5
wdxtub <wdxtub@qq.com>
2019-04-17 20:00:32 +0800

    add tensorboard

--- python_deep_learning/7_advance_practice/3_three_output_model.py
+++ python_deep_learning/7_advance_practice/3_three_output_model.py
@@ -12,3 +12,42 @@ x = layers.Conv1D(128, 5, activation='relu')(embedded_posts)
 x = layers.MaxPooling1D(5)(x)
 x = layers.Conv1D(256, 5, activation='relu')(x)
 x = layers.Conv1D(256, 5, activation='relu')(x)
+x = layers.MaxPooling1D(5)(x)
+x = layers.Conv1D(256, 5, activation='relu')(x)
+x = layers.Conv1D(256, 5, activation='relu')(x)
+x = layers.GlobalMaxPooling1D()(x)
+x = layers.Dense(128, activation='relu')(x)
+
+age_prediciton = layers.Dense(1, name='age')(x)
+income_prediction = layers.Dense(num_income_groups, activation='softmax', name='income')(x)
+gender_prediction = layers.Dense(1, activation='sigmoid', name='gender')(x)
+
+model = Model(posts_input, [age_prediciton, income_prediction, gender_prediction])
+print("带权重的多重损失，这样能更好平衡多个任务")
+output_has_name = True
+if output_has_name:
+    model.compile(optimizer='rmsprop', 
+        loss={
+            'age': 'mse', 
+            'income': 'categorical_crossentropy', 
+            'gender': 'binary_crossentropy'},
+        loss_weights={
+            'age': 0.25,
+            'income': 1.,
+            'gender': 10.
+        })
+else:
+    model.compile(optimizer='rmsprop', 
+        loss=['mse', 'categorical_crossentropy', 'binary_crossentropy'],
+        loss_weights=[0.25, 1., 10.])
+
+print("假设 xxx_targets 为 Numpy 数组，把输入给到模型中")
+if output_has_name:
+    model.fit(posts, {
+        'age': age_targets,
+        'income': income_targets,
+        'gender': gender_targets
+    }, epochs=10, batch_size=64)
+else:
+    model.fit(posts, [age_targets, income_targets, gender_targets],
+    epochs=10, batch_size=64)
---
+++ python_deep_learning/7_advance_practice/4_keras_tensorboard.py
@@ -0,0 +1,33 @@
+import keras
+from keras import layers
+from keras.datasets import imdb
+from keras.preprocessing import sequence
+
+max_features = 2000
+max_len = 500
+
+print("载入数据")
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+x_train = sequence.pad_sequences(x_train, maxlen=max_len)
+x_test = sequence.pad_sequences(x_test, maxlen=max_len)
+print("构造模型")
+model = keras.models.Sequential()
+model.add(layers.Embedding(max_features, 128, input_length=max_len, name='embed'))
+model.add(layers.Conv1D(32, 7, activation='relu'))
+model.add(layers.MaxPooling1D(5))
+model.add(layers.Conv1D(32, 7, activation='relu'))
+model.add(layers.GlobalMaxPooling1D())
+model.add(layers.Dense(1))
+print("模型结构")
+print(model.summary())
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+print("添加回调函数记录信息")
+callbacks = [
+    keras.callbacks.TensorBoard(
+        log_dir='log', # 日志写入位置
+        histogram_freq=1, # 每一轮都记录激活直方图
+        embeddings_freq=1, # 每一轮之后记录嵌入数据
+    )
+]
+history = model.fit(x_train, y_train, epochs=20, batch_size=128, validation_split=0.2, callbacks=callbacks)
+print("使用 tensorboard --logdir=log 启动，访问 localhost:6006")
