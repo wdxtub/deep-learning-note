2603e827d78e06d094001e3902975dcd0997aadb
dawang <wdxtub@qq.com>
2020-05-20 19:50:20 +0800

    add phe test

--- phe_test/spark_test.py
+++ phe_test/spark_test.py
@@ -1,3 +1,4 @@
+from pyspark import SparkContext
 from phe import paillier
 import numpy as np
 import datetime
@@ -5,7 +6,7 @@ import datetime
 # 向量维度为 128
 # vcount = 100000
 # 测试用 1000 个 
-vcount = 5000
+vcount = 20
 vdim = 128
 # phe 加密位数
 encrypt_bit = 1024
@@ -24,20 +25,31 @@ encrypted_float_c = public_key.encrypt(float_c)
 print(f'[encrpyted float c] {encrypted_float_c}')
 print(f'[decrypted float c] {private_key.decrypt(encrypted_float_c)}')
 
-vector_list = np.random.rand(vcount, vdim)
+vector_list = np.ones((vcount, vdim))
 print(f'generate raw numpy array with shape {vector_list.shape}')
 
+print('loading vector list to rdd')
+sc = SparkContext("local", "phe test")
+raw_vector_rdd = sc.parallelize(vector_list)
 tstart = datetime.datetime.now()
 print(f'[start time] {tstart}')
-enc_vector_list = np.dot(encrypted_float_c, vector_list)
+
+def encrypt_mul(array):
+    return np.dot(encrypted_float_c, array)
+
+# 注意，实际执行时，这里是 lazy 的
+enc_vector_rdd = raw_vector_rdd.map(encrypt_mul)
 tend = datetime.datetime.now()
 print(f'[end time] {tend}')
-print(f'encrypted array with shape {enc_vector_list.shape}')
+print(f'encrypted array done')
 print(f'[duration] {tend-tstart}s')
-# vector 相加
-enc_result = np.sum(enc_vector_list, axis=0)
+# print(enc_vector_rdd.take(2))
+final_result = enc_vector_rdd.sum()
+# print(final_result)
 tend = datetime.datetime.now()
 print(f'[end time] {tend}')
-print(f'sum encrypted array with shape {enc_result.shape}')
+print(f'sum encrypted array done')
 print(f'[duration] {tend-tstart}s')
-print('all done')
+decrpyt_result = [private_key.decrypt(x) for x in final_result]
+print(decrpyt_result)
+print("all done")
